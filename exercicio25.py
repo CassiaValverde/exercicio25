33c33b8eb841f2ca7e39edd099f7521279cadc2a
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-10-16 20:37:48 -0300

    Add files via upload

---
+++ exercicio25.py
@@ -0,0 +1,6 @@
+#formula de bhaskara
+a = float(input("Informe o coficiente A "))
+b = float(input("Informe o coficiente B "))
+c = float(input("Informe o coficiente C "))
+delta = (b*b)-4*(a*c)
+print(delta)
